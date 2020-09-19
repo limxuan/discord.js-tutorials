3bed49118e0730280447a44f1fbb11b10417bcbd
recon <66986299+reconlx@users.noreply.github.com>
2020-09-19 20:44:37 +0800

    Command Handler for Discord.js

---
+++ commands/info/ping.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name : 'ping',
+    category : 'info',
+    description : 'Returns latency and API ping',
+
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+
+    run : async(client, message, args) => {
+        const msg = await message.channel.send(`🏓 Pinging...`)
+        const embed = new MessageEmbed()
+            .setTitle('Pong!')
+            .setDescription(`WebSocket ping is ${client.ws.ping}MS\nMessage edit ping is ${Math.floor(msg.createdAt - message.createdAt)}MS!`)
+            await message.channel.send(embed)
+            msg.delete()
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const {Collection, Client, Discord} = require('discord.js')
+const fs = require('fs')
+const client = new Client({
+    disableEveryone: true
+})
+const config = require('./config.json')
+const prefix = config.prefix
+const token = config.token
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+}); 
+client.on('ready', () => {
+    client.user.setActivity(`${prefix}help`)
+    console.log(`${client.user.username} ✅`)
+})
+client.on('message', async message =>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd.length == 0 ) return;
+    const command = client.commands.get(cmd)
+    if(!command) command = client.commands.get(bot.aliases.get(cmd));
+    if(command) command.run(client, message, args) 
+})
+client.login(token)
