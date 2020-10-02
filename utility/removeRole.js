d3cf57a62a647c9daa42b678d82846c3a3c4438b
recon <66986299+reconlx@users.noreply.github.com>
2020-10-03 01:34:24 +0800

    Add files via upload

---
+++ utility/removeRole.js
@@ -0,0 +1,22 @@
+const { Message } = require('discord.js')
+
+module.exports = {
+    name : 'removerole',
+    run : async(client, message, args) => {
+        //lets use parameters (optional)
+        /**
+         * @param {Message} message
+         */
+        //so firstly we will check whether the author of the message has permissions
+        //this line means if the author doesn't have manage roles permission it will stop the process and send the following text
+        if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You do not have permission.')
+        //next we define some variables
+        const target = message.mentions.members.first() //member = mentions
+        if(!target) return message.channel.send('No member specified') //when no member is pinged
+        const role = message.mentions.roles.first() // roles = mentions
+        if(!role) return message.channel.send('No role specified') //when no role is specified or pinged
+        //now the code!
+        await target.roles.remove(role) // removeing the role to the user
+        message.channel.send(`${target.user.username} roles has been removed`) //this is optional and editable
+    }
+}
