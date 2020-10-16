ce580aa7521dd78b0247f4762182bf4f35f58345
recon <66986299+reconlx@users.noreply.github.com>
2020-10-16 09:58:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ client.on('message', async message =>{
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     if(cmd.length == 0 ) return;
-    const command = client.commands.get(cmd)
-    if(!command) command = client.commands.get(bot.aliases.get(cmd));
+    let command = client.commands.get(cmd)
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
     if(command) command.run(client, message, args) 
 })
 client.login(token)
