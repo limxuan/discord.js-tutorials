65a2febbd0d50c3c4c3672ba02bf19574d7a69ba
recon <66986299+reconlx@users.noreply.github.com>
2020-10-20 07:48:34 +0800

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name : 'clear',
+    aliases : ['purge'],
+    run : async(client, message, args) => {
+        if(!args[0]) return message.channel.send('Please specify a number of messages to delete ranging from 1 - 99')
+        if(isNaN(args[0])) return message.channel.send('Numbers are only allowed')
+        if(parseInt(args[0]) > 99) return message.channel.send('The max amount of messages that I can delete is 99')
+        await message.channel.bulkDelete(parseInt(args[0]) + 1)
+            .catch(err => console.log(err))
+        message.channel.send('Deleted ' + args[0]  + " messages.")
+    }
+}
