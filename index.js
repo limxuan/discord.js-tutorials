da56e891b82fb0d925b0b45e19f87fd3349f9db2
recon <66986299+reconlx@users.noreply.github.com>
2020-10-31 12:08:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+client.on('message', async(message) => {
+    if(message.author.bot) return;
+    if(usersMap.has(message.author.id)) {
+        const userData = usersMap.get(message.author.id);
+        const { lastMessage, timer } = userData;
+        const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+        let msgCount = userData.msgCount;
+        console.log(difference);
+
+        if(difference > DIFF) {
+            clearTimeout(timer);
+            console.log('Cleared Timeout');
+            userData.msgCount = 1;
+            userData.lastMessage = message;
+            userData.timer = setTimeout(() => {
+                usersMap.delete(message.author.id);
+                console.log('Removed from map.')
+            }, TIME);
+            usersMap.set(message.author.id, userData)
+        }
+        else {
+            ++msgCount;
+            if(parseInt(msgCount) === LIMIT) {
+                let muterole = message.guild.roles.cache.find(role => role.name === 'muted');
+                if(!muterole) {
+                    try{
+                        muterole = await message.guild.roles.create({
+                            name : "muted",
+                            permissions: []
+                        })
+                        message.guild.channels.cache.forEach(async (channel, id) => {
+                            await channel.createOverwrite(muterole, {
+                                SEND_MESSAGES: false,
+                                ADD_REACTIONS : false
+                            })
+                        })
+                    }catch (e) {
+                        console.log(e)
+                    }
+                }
+                message.member.roles.add(muterole);
+                message.channel.send('You have been muted!');
+                setTimeout(() => {
+                    message.member.roles.remove(muterole);
+                    message.channel.send('You have been unmuted!')
+                }, TIME);
+            } else {
+                userData.msgCount = msgCount;
+                usersMap.set(message.author.id, userData);
+            }
+        }
+    }
+    else {
+        let fn = setTimeout(() => {
+            usersMap.delete(message.author.id);
+            console.log('Removed from map.')
+        }, TIME);
+        usersMap.set(message.author.id, {
+            msgCount: 1,
+            lastMessage : message,
+            timer : fn
+        });
+    }
+})
