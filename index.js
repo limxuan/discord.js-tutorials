272eaaa738ccca1aeedc3a210cc2b738caec2b82
recon <66986299+reconlx@users.noreply.github.com>
2020-11-03 23:16:30 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+client.on('message', async(message) => {
+    if(message.channel.parentID !== '756890395392081985') return;
+    client.ticketTranscript.findOne({ Channel : message.channel.id }, async(err, data) => {
+        if(err) throw err;
+        if(data) {
+           console.log('there is data')
+           data.Content.push(`${message.author.tag} : ${message.content}`) 
+        } else {
+            console.log('there is no data')
+            data = new client.ticketTranscript({ Channel : message.channel.id, Content: `${message.author.tag} : ${message.content}`})
+        }
+        await data.save()
+            .catch(err =>  console.log(err))
+        console.log('data is saved ')
+    })
+
+})
