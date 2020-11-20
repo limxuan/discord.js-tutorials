e07394f642183b63a2797201fc243e698dfdc471
recon <66986299+reconlx@users.noreply.github.com>
2020-11-20 22:20:46 +0800

    Create hangman.js

---
+++ hangman.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name : 'hangman',
+    run : async(client, message, args) => {
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You need manage messages permission.')
+        const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+        if(!channel) return message.channel.send('Please specify a channel')
+        const word = args.slice(1).join(" ")
+        if(!word) return  message.channel.send('Please specify a word to guess.')
+
+        const hang = new hangman({
+            message: message,
+            word: word,
+            client: client,
+            channelID: channel.id,
+        })
+
+        hang.start();
+    }
+}
