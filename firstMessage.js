ab8067ebb4bcc9ccc90a7c7bf684ca8452d36d1a
recon <olimxuan2005@gmail.com>
2021-01-13 11:12:03 +0800

    Add files via upload

---
+++ firstMessage.js
@@ -0,0 +1,28 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "firstmessage",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const fetchMessages = await message.channel.messages.fetch({
+      after: 1,
+      limit: 1,
+    });
+    const msg = fetchMessages.first();
+
+    message.channel.send(
+      new MessageEmbed()
+        .setTitle(`First Messsage in ${message.guild.name}`)
+        .setURL(msg.url)
+        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+        .setDescription("Content: " + msg.content)
+        .addField("Author", msg.author, true)
+        .addField('Message ID', msg.id, true)
+        .addField('Created At', message.createdAt.toLocaleDateString(), true)
+    );
+  },
+};
