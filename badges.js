61db3f6834d66e963626b609fbd1ffc585888f22
recon <olimxuan2005@gmail.com>
2021-01-15 11:17:48 +0800

    Add files via upload

---
+++ badges.js
@@ -0,0 +1,19 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'badges',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        const user = message.mentions.users.first() || message.author;
+
+        const flags = user.flags.toArray();
+
+        console.log(flags);
+        
+        message.channel.send(`${user}'s badges: ${flags.join(', ')}`)
+    }
+}
