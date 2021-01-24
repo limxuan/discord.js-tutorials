da50e9bada23bbf7a8efbdce65a9955cbbc30570
recon <olimxuan2005@gmail.com>
2021-01-25 05:45:41 +0800

    Update set-lang.js

--- set-lang.js
+++ set-lang.js
@@ -1,12 +1,24 @@
-const  db = require('../../reconDB');
-
+const { Client, Message, MessageEmbed } = require("discord.js");
+const axios = require("axios");
 module.exports = {
-    name: 'set-lang',
-    run: async(client, message, args) => {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return;
-        const lang = args[0];
-        if(!lang) message.reply('please specify a message');
-        await db.set(`lang-${message.guild.id}`, lang);
-        message.channel.send('Language has been set to ' + lang);
-    }
-}
+  name: "djs",
+  aliases: ["docs"],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const query = args.join(" ");
+    if (!query) return message.reply("Please specify a query!");
+    const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+      query
+    )}`;
+
+    axios.get(url).then(({ data }) => {
+      if (data) {
+        message.channel.send({ embed: data });
+      }
+    });
+  },
+};
